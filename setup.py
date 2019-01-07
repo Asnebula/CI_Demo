3516a6237b02c0e6fa82438c2e50cf76db70f6ba
Asnebula <Asenbula@sina.com>
2019-01-07 14:41:03 +0800

    add file(not fully necessary)

--- setup.py
+++ setup.py
@@ -5,7 +5,20 @@ from setuptools import (
     setup,
 )
 
+classifiers = [
+    "Development Status :: 4 - Beta",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.4",
+    "Programming Language :: Python :: 3.5",
+    "License :: OSI Approved :: Apache Software License",
+    "Intended Audience :: Science/Research",
+    "Topic :: Scientific/Engineering",
+    "Topic :: Scientific/Engineering :: Mathematics",
+    "Operating System :: OS Independent"
+]
 LICENSE = "Apache License, Version 2.0"
+
 if __name__ == '__main__':
     setup(
         name='demo',
@@ -13,5 +26,6 @@ if __name__ == '__main__':
         version='0.0.1',
         author=__author__,
         license=LICENSE,
+        classifiers=classifiers,
         packages=['my_pkg'],
     )
